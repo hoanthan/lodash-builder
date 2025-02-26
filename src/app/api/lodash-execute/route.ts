e604cbec0726086d85e55f9906a5b6d18af187fc
Hoan Than <hoanthan317@gmail.com>
2025-02-26 14:09:30 +0700

    type fix

--- src/app/api/lodash-execute/route.ts
+++ src/app/api/lodash-execute/route.ts
@@ -10,7 +10,8 @@ export async function POST(request: Request) {
     const func = new Function("_", `${code} return result;`)
     const result = func(_)
     return NextResponse.json(result)
-  } catch (error) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 })
   }
 }
